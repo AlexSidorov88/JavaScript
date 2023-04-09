eae767514e38c29e1dd5ec50f1c56cfd6b315323
Alex <105877232+AlexSidorov88@users.noreply.github.com>
2023-04-09 13:27:43 +0200

    Add files via upload

---
+++ Cw_Even or Odd.js
@@ -0,0 +1,7 @@
+function evenOrOdd(number) {
+   if (number % 2 == 0) return "Even"
+   else return "Odd";
+ }
+
+ /*Create a function that takes an integer as an argument and returns 
+  "Even" for even numbers or "Odd" for odd numbers.*/
