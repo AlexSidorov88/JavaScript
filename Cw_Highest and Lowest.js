50527bcc22542cd1a2a6dd51b75d5ba52ac15c9f
Alex <105877232+AlexSidorov88@users.noreply.github.com>
2023-04-10 18:39:49 +0200

    Add files via upload

---
+++ Cw_Highest and Lowest.js
@@ -0,0 +1,12 @@
+function highAndLow(numbers){
+   var x = numbers.split(' ').map(Number)
+      return Math.max(...x) + " " + Math.min(...x)
+
+ }
+
+
+/*In this little assignment you are given a string of space separated numbers,
+   and have to return the highest and lowest number.
+   All numbers are valid Int32, no need to validate them.
+There will always be at least one number in the input string.
+Output string must be two numbers separated by a single space, and highest number is first.*/
